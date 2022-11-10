159aee881ca3ecf26c9b57915de8d54d238e0ddd
Lucas <lgxm3z@gmail.com>
2022-11-10 05:10:43 -0300

    fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import makeStyleFrom from './makeStyleFrom';
 
+// Components
 import _Text from './components/Text';
+import _View from './components/View';
 
-export {makeStyleFrom, _Text as Text};
+export {makeStyleFrom, _Text as Text, _View as View};
