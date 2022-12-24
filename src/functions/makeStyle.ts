91a7554d14aa1ab7a11fd222f3e481934ddb1ae9
Lucas <lgxm3z@gmail.com>
2022-12-24 15:32:47 -0300

    change tsconfig

--- src/functions/makeStyle.ts
+++ src/functions/makeStyle.ts
@@ -2,6 +2,7 @@ import React from "react";
 import type { Element, ElementCallback } from "../interfaces/Element";
 
 const makeStyle = (callback: ElementCallback) => {
+  //@ts-ignore
   style: {
   }
 };
